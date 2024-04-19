caf32daec072ee069372f7ded96c8e55935bbd01
Kasun-Abeywickrama <121012573+Kasun-Abeywickrama@users.noreply.github.com>
2024-04-19 17:27:49 +0530

    Uploading Read orders and Read details of a particular order

---
+++ E-Commerce-Website/src/java/OrderManagement/OrderDAO.java
@@ -0,0 +1,196 @@
+package OrderManagement;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import DatabaseConnection.DBConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author HP
+ */
+public class OrderDAO {
+
+    private static final String GET_ALL_ORDERS_SQL = "SELECT * FROM orders";
+    private static final String GET_ORDER_BY_ID_SQL = "SELECT * FROM orders WHERE order_id = ?";
+    private static final String ADD_ORDER_SQL = "INSERT INTO orders (date_time, total_price, status, shipping_address, receiver_name, receiver_phone_number, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_ORDER_SQL = "UPDATE orders SET date_time = ?, total_price = ?, status = ?, shipping_address = ?, receiver_name = ?, receiver_phone_Number = ?, user_id = ? WHERE order_id = ?";
+    private static final String DELETE_ORDER_SQL = "DELETE FROM orders WHERE order_id = ?";
+    private static final String GET_ORDERS_BY_STATUS_SQL = "SELECT * FROM orders WHERE status = ?";
+    private static final String UPDATE_ORDER_STATUS_SQL = "UPDATE orders SET status = ? WHERE order_id = ?";
+
+
+    public static List<Order> getOrders() throws SQLException {
+        List<Order> orders = new ArrayList<>();
+        DBConnectionManager cob = new DBConnectionManager();
+        Connection connection = cob.getDBConnection();
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement(GET_ALL_ORDERS_SQL);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int orderId = resultSet.getInt("order_id");
+                String dateTime = resultSet.getString("date_time");
+                double totalPrice = resultSet.getDouble("total_price");
+                String status = resultSet.getString("status");
+                String shippingAddress = resultSet.getString("shipping_address");
+                String receiverName = resultSet.getString("receiver_name");
+                String receiverPhoneNumber = resultSet.getString("receiver_phone_number");
+                int userId = resultSet.getInt("user_id");
+
+                orders.add(new Order(orderId, dateTime, totalPrice, status, shippingAddress, receiverName, receiverPhoneNumber, userId));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error getting orders: " + e);
+            throw e; // Re-throw the exception for proper handling
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    cob.closeDBConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+            }
+        }
+        return orders;
+    }
+
+    public static Order getOrderById(int orderId) throws SQLException {
+        DBConnectionManager cob = new DBConnectionManager();
+        Connection connection = cob.getDBConnection();
+        PreparedStatement statement = null;
+        Order order = null;
+
+        try {
+            statement = connection.prepareStatement(GET_ORDER_BY_ID_SQL);
+            statement.setInt(1, orderId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String dateTime = resultSet.getString("date_time");
+                double totalPrice = resultSet.getDouble("total_price");
+                String status = resultSet.getString("status");
+                String shippingAddress = resultSet.getString("shipping_address");
+                String receiverName = resultSet.getString("receiver_name");
+                String receiverPhoneNumber = resultSet.getString("receiver_phone_number");
+                int userId = resultSet.getInt("user_id");
+
+                order = new Order(orderId, dateTime, totalPrice, status, shippingAddress, receiverName, receiverPhoneNumber, userId);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error getting order by ID: " + e);
+            throw e; // Re-throw the exception for proper handling
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    cob.closeDBConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+            }
+        }
+        return order;
+    }
+
+    public static List<Order> getOrdersByStatus(String status) throws SQLException {
+        List<Order> orders = new ArrayList<>();
+        DBConnectionManager cob = new DBConnectionManager();
+        Connection connection = cob.getDBConnection();
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement(GET_ORDERS_BY_STATUS_SQL);
+            statement.setString(1, status);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int orderId = resultSet.getInt("order_id");
+                String dateTime = resultSet.getString("date_time");
+                double totalPrice = resultSet.getDouble("total_price");
+                String orderStatus = resultSet.getString("status");
+                String shippingAddress = resultSet.getString("shipping_address");
+                String receiverName = resultSet.getString("receiver_name");
+                String receiverPhoneNumber = resultSet.getString("receiver_phone_number");
+                int userId = resultSet.getInt("user_id");
+
+                orders.add(new Order(orderId, dateTime, totalPrice, orderStatus, shippingAddress, receiverName, receiverPhoneNumber, userId));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error getting orders by status: " + e);
+            throw e; // Re-throw the exception for proper handling
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    cob.closeDBConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+            }
+        }
+        return orders;
+    }
+
+    public static int addOrder(Order order) throws SQLException {
+        DBConnectionManager cob = new DBConnectionManager();
+        Connection connection = cob.getDBConnection();
+        PreparedStatement statement = null;
+        int newId = 0;
+
+        try {
+            statement = connection.prepareStatement(ADD_ORDER_SQL, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, order.getDateTime());
+            statement.setDouble(2, order.getTotalPrice());
+            statement.setString(3, order.getStatus());
+            statement.setString(4, order.getShippingAddress());
+            statement.setString(5, order.getReceiverName());
+            statement.setString(6, order.getReceiverPhoneNumber());
+            statement.setInt(7, order.getUserId());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    // Get the ID of the newly inserted row
+                    newId = generatedKeys.getInt(1);
+                } else {
+                    System.out.println("No auto-generated keys were returned");
+                }
+            } else {
+                System.out.println("Order insertion failed");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error adding order: " + e);
+            throw e; // Re-throw the exception for proper handling
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    cob.closeDBConnection();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+            }
+        }
+        return newId;
+    }
+
+
+}
---
+++ E-Commerce-Website/web/orders_page.jsp
@@ -0,0 +1,181 @@
+<%-- 
+    Document   : orders_page
+    Created on : Apr 17, 2024, 1:37:19 PM
+    Author     : HP
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%@ include file="admin_header.jsp" %>
+
+<style>
+    .nav-link{
+        color: black;
+    }
+
+    .nav-link:hover{
+        color: #E97000;
+    }
+
+    .active{
+        background-color: #E97000 !important;
+    }
+    .nav-link{
+        padding: 4px 90px;
+    }
+</style>
+
+<div class="container">
+
+    <p style="color: #E97000;"><i>Orders &nbsp;>&nbsp; New Orders </i></p>  
+
+
+    <div class="w-100">    
+        <ul class="nav nav-tabs">
+            <li class="nav-item" onclick="getPendingOrders()">
+                <a class="nav-link active" href="#pending">Pending</a>
+            </li>
+            <li class="nav-item" onclick="getDispatchedOrders()">
+                <a class="nav-link" href="#dispatched">Dispatched</a>
+            </li>
+            <li class="nav-item" onclick="getDeliveredOrders()">
+                <a class="nav-link" href="#delivered">Delivered</a>
+            </li>
+            <li class="nav-item" onclick="getAllOrders()">
+                <a class="nav-link" href="#all">All</a>
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="w-100 mt-4">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Order Id</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Total price</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+</div>
+
+<script>
+    function setActiveTab(tabName) {
+        var tabs = document.querySelectorAll('.nav-link');
+        tabs.forEach(function (tab) {
+            tab.classList.remove('active');
+        });
+
+        var activeTab = document.querySelector('a[href="#' + tabName + '"]');
+        activeTab.classList.add('active');
+    }
+
+    function displayOrders(orders) {
+        var tbody = document.querySelector("tbody");
+
+        tbody.innerHTML = "";
+
+        orders.forEach(function (order) {
+            var row = document.createElement("tr");
+
+            var idCell = document.createElement("td");
+            idCell.textContent = order.orderId;
+            row.appendChild(idCell);
+
+            var dateTimeCell = document.createElement("td");
+            dateTimeCell.textContent = order.dateTime;
+            row.appendChild(dateTimeCell);
+
+            var totalPriceCell = document.createElement("td");
+            totalPriceCell.textContent = order.totalPrice;
+            row.appendChild(totalPriceCell);
+
+            var actionCell = document.createElement("td");
+            var viewLink = document.createElement("a");
+            viewLink.href = "order_page.jsp?order_id=" + order.orderId;
+            viewLink.textContent = "View";
+            actionCell.appendChild(viewLink);
+            row.appendChild(actionCell);
+
+            tbody.appendChild(row);
+        });
+    }
+
+    function getPendingOrders() {
+        setActiveTab('pending');
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "ReadOrderServlet?orderStatus=Pending", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                displayOrders(responseData);
+            }
+        };
+        xhr.send();
+    }
+
+    getPendingOrders();
+</script>
+
+<script>
+    function getDispatchedOrders() {
+        setActiveTab('dispatched');
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "ReadOrderServlet?orderStatus=Dispatched", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                displayOrders(responseData);
+            }
+        };
+        xhr.send();
+    }
+</script>
+
+<script>
+    function getDeliveredOrders() {
+        setActiveTab('delivered');
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "ReadOrderServlet?orderStatus=Delivered", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                displayOrders(responseData);
+            }
+        };
+        xhr.send();
+    }
+</script>
+
+<script>
+    function getAllOrders() {
+        setActiveTab('all');
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "ReadOrderServlet", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                displayOrders(responseData);
+            }
+        };
+        xhr.send();
+    }
+</script>
+
+<%@ include file="admin_footer.jsp" %>
+
